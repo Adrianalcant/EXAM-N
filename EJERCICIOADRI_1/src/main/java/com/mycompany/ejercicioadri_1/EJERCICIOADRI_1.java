e213793d05b6adadb81ba64249f3248546b764cb
ASUS <ASUS@LAPTOP-SKF8L1RA>
2025-05-26 09:40:30 -0500

    hola

---
+++ EJERCICIOADRI_1/src/main/java/com/mycompany/ejercicioadri_1/EJERCICIOADRI_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicioadri_1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class EJERCICIOADRI_1 {
+
+    public static void main(String[] args) {
+        System.out.println("mostrar edad!");
+    }
+}
+/*commit = mostrar el cmabio
+push= subir el cambio que se hace en el comit
+pull= cargar la vesión actual del proyecto
+*/
